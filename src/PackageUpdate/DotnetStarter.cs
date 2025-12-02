ca9e4cbcce54ad27915d5ed287d8dc535a0ba0d2
Simon Cropp <simon.cropp@gmail.com>
2025-12-02 11:51:12 +1100

    Create DotnetStarter.cs

---
+++ src/PackageUpdate/DotnetStarter.cs
@@ -0,0 +1,52 @@
+﻿static class DotnetStarter
+{
+    public static async Task<List<string>> StartDotNet(string arguments, string directory, int timeout)
+    {
+        using var process = new Process
+        {
+            StartInfo = new()
+            {
+                FileName = "dotnet",
+                Arguments = arguments,
+                WorkingDirectory = directory,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            }
+        };
+        process.Start();
+        Log.Information("    dotnet {Arguments}", arguments);
+
+        if (timeout == 0)
+        {
+            return [];
+        }
+
+        if (!process.WaitForExit(timeout))
+        {
+            throw new(
+                $"""
+                 Command: dotnet {arguments}
+                 Timed out
+                 WorkingDirectory: {directory}
+                 """);
+        }
+
+        if (process.ExitCode == 0)
+        {
+            return await process.ReadLines();
+        }
+
+        var error = await process.StandardError.ReadToEndAsync();
+        var output = await process.StandardOutput.ReadToEndAsync();
+        throw new(
+            $"""
+             Command: dotnet {arguments}
+             WorkingDirectory: {directory}
+             ExitCode: {process.ExitCode}
+             Error: {error}
+             Output: {output}
+             """);
+    }
+}
