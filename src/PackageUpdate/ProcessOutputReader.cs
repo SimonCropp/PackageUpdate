1dd9d4b8c7de50dd83b310c61a01a3bf9d7af86b
Simon Cropp <simon.cropp@gmail.com>
2025-12-02 11:52:42 +1100

    Create ProcessOutputReader.cs

---
+++ src/PackageUpdate/ProcessOutputReader.cs
@@ -0,0 +1,17 @@
+﻿public static class ProcessOutputReader
+{
+    public static async Task<List<string>> ReadLines(this Process process)
+    {
+        var list = new List<string>();
+        while (await process.StandardOutput.ReadLineAsync() is { } line)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                continue;
+            }
+            list.Add(line);
+        }
+
+        return list;
+    }
+}
