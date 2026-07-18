761a7223041837b81464f99db1b5a48d7fb28672
Simon Cropp <simon.cropp@gmail.com>
2026-07-18 21:54:46 +1000

    Update Formatter.cs

--- src/PackageUpdate/Formatter.cs
+++ src/PackageUpdate/Formatter.cs
@@ -1,11 +1,22 @@
 static class Formatter
 {
-    public static string FormatElapsed(TimeSpan elapsed) =>
-        elapsed.TotalHours >= 1
-            ? $"{(int) elapsed.TotalHours}h{elapsed.Minutes}m"
-            : elapsed.TotalMinutes >= 1
-                ? $"{(int) elapsed.TotalMinutes}m{elapsed.Seconds}s"
-                : elapsed.TotalSeconds >= 1
-                    ? $"{elapsed.TotalSeconds:0.0}s"
-                    : $"{elapsed.TotalMilliseconds:0}ms";
+    public static string FormatElapsed(TimeSpan elapsed)
+    {
+        if (elapsed.TotalHours >= 1)
+        {
+            return $"{(int) elapsed.TotalHours}h{elapsed.Minutes}m";
+        }
+
+        if (elapsed.TotalMinutes >= 1)
+        {
+            return $"{(int) elapsed.TotalMinutes}m{elapsed.Seconds}s";
+        }
+
+        if (elapsed.TotalSeconds >= 1)
+        {
+            return $"{elapsed.TotalSeconds:0.0}s";
+        }
+
+        return $"{elapsed.TotalMilliseconds:0}ms";
+    }
 }
