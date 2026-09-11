37a384a212a3fa5c96331d6a0d103ca36c301ea6
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-09-11 14:04:20 +0100

    add helper and tests

---
+++ src/Tests/Helpers/PropsHelper.cs
@@ -0,0 +1,16 @@
+internal static class PropsHelper
+{
+    internal static string CreateSolutionDir(string root, string relativePath)
+    {
+        var dir = Path.Combine(root, relativePath);
+        Directory.CreateDirectory(dir);
+        return dir;
+    }
+
+    internal static async Task<string> CreateProps(string directory)
+    {
+        var propsPath = Path.Combine(directory, "Directory.Packages.props");
+        await File.WriteAllTextAsync(propsPath, "<Project />");
+        return propsPath;
+    }
+}
