f3c0597ffde0786701c12adf0f834735e58e2cc1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-09-11 13:57:47 +0100

    move TempDir helper to shared directory

--- src/Tests/ForkDetectorTests.cs
+++ src/Tests/ForkDetectorTests.cs
@@ -148,21 +148,4 @@ public class ForkDetectorTests
         File.WriteAllText(Path.Combine(gitDir, "config"), configContent);
     }
 
-    sealed class TempDir :
-        IDisposable
-    {
-        public string Path { get; }
-
-        public TempDir()
-        {
-            Path = System.IO.Path.Combine(
-                System.IO.Path.GetTempPath(),
-                "PackageUpdateTests",
-                Guid.NewGuid().ToString());
-            Directory.CreateDirectory(Path);
-        }
-
-        public void Dispose() =>
-            Directory.Delete(Path, true);
-    }
 }
